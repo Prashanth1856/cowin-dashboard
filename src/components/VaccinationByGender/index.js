6f1c71cbb6a2624c67cc78e77517c76f2600829d
PrashuPal <prashanthpotragalla@gmail.com>
2022-10-28 06:58:42 +0000

    cowin

---
+++ src/components/VaccinationByGender/index.js
@@ -0,0 +1,39 @@
+// Write your code here
+import {PieChart, Pie, Cell, Legend} from 'recharts'
+
+import './index.css'
+
+const VaccinationByGender = props => {
+  const {vaccinationByGenderDetails} = props
+
+  return (
+    <div className="vbg-container">
+      <h1 className="vbg-heading">Vaccination by gender</h1>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="60%"
+          data={vaccinationByGenderDetails}
+          startAngle={180}
+          endAngle={0}
+          dataKey="count"
+          innerRadius="30%"
+          outerRadius="60%"
+        >
+          <Cell name="Male" fill="#f54394" />
+          <Cell name="Female" fill="#5a8dee" />
+          <Cell name="Others" fill="#2cc6c6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+          wrapperStyle={{fontSize: 12, fontFamily: 'Roboto'}}
+        />
+      </PieChart>
+    </div>
+  )
+}
+
+export default VaccinationByGender
